c31dae3128bb29ccf7ab6d22e0a299c9f01ef014
LeonAsllani <122211787+LeonAsllani@users.noreply.github.com>
2023-12-10 21:04:06 +0100

    Add files via upload

---
+++ Rent-a-car/register.js
@@ -0,0 +1,43 @@
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+
+const forms = document.querySelector(".forms"),
+      pwShowHide = document.querySelectorAll(".eye-icon"),
+      links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+    })
+})
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        forms.classList.toggle("show-signup");
+    })
+})
+
